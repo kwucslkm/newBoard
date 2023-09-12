aa091630a815432ce8769772e7ccec93487d7f10
codingrecipe1 <codingrecipe1@gmail.com>
2023-09-12 09:12:41 +0900

    04_글조회

--- src/main/java/com/icia/board/controller/BoardController.java
+++ src/main/java/com/icia/board/controller/BoardController.java
@@ -5,10 +5,7 @@ import com.icia.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -37,6 +34,17 @@ public class BoardController {
         model.addAttribute("boardList", boardDTOList);
         return "boardPages/boardList";
     }
+
+    @GetMapping
+    public String findById(@RequestParam("id") Long id, Model model) {
+        // 조회수 처리
+        // 데이터 가져오기
+        boardService.updateHits(id);
+        BoardDTO boardDTO = boardService.findById(id);
+        model.addAttribute("board", boardDTO);
+        return "boardPages/boardDetail";
+    }
+
 
 
 
---
+++ src/main/webapp/WEB-INF/views/boardPages/boardDetail.jsp
@@ -0,0 +1,38 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<div id="section">
+    <table>
+        <tr>
+            <th>id</th>
+            <td>${board.id}</td>
+        <tr>
+            <th>writer</th>
+            <td>${board.boardWriter}</td>
+        </tr>
+        <tr>
+            <th>date</th>
+            <td>${board.createdAt}</td>
+        </tr>
+        <tr>
+            <th>hits</th>
+            <td>${board.boardHits}</td>
+        </tr>
+        <tr>
+            <th>title</th>
+            <td>${board.boardTitle}</td>
+        </tr>
+        <tr>
+            <th>contents</th>
+            <td>${board.boardContents}</td>
+        </tr>
+    </table>
+    <button onclick="board_list()">목록</button>
+    <button onclick="board_update()">수정</button>
+    <button onclick="board_delete()">삭제</button>
+</div>
+</body>
+</html>
